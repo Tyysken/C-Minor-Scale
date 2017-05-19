eabc951b59e58c0254163cbd335725882fc79b39
Henrik Ottehall <henot294@gmail.com>
2017-05-19 09:39:01 +0200

    Fixed JQuery doubling. Added DataTables to MyBookings

--- C-Minor-Scale/Scripts/MyBookings.js
+++ C-Minor-Scale/Scripts/MyBookings.js
@@ -19,6 +19,7 @@ function updateBookingsTable() {
                 $("<tr><td id=\"" + tdid + "\"></td><td>" + bookings[i].Subject + "</td><td>" + moment.utc(bookings[i].From).tz("Europe/Stockholm").format("YYYY-MM-DD HH:mm") + "</td><td>" + moment.utc(bookings[i].Until).tz("Europe/Stockholm").format("YYYY-MM-DD HH:mm") + "</td>").appendTo("#bookingsTbody");
                 updateBookingsTableEntry(tdid, bookings[i].Zid);
             }
+            $('#bookingsTable').DataTable();
         },
         error: function (data) {
             alert(JSON.parse(data.responseText).Message);
